8f69eb1d2ea38b936409c648b4a43ab98c6af41b
Li Ma <31489620+LiCracker@users.noreply.github.com>
2020-06-09 18:27:39 -0700

    Fix code and readme

--- PySparkAIPapersGrowthComparison.py
+++ PySparkAIPapersGrowthComparison.py
@@ -10,12 +10,12 @@ MagContainer = '<MagContainer>'                   # The container name in Azure 
 
 # COMMAND ----------
 
-# Create a MicrosoftAcademicGraph instance to access MAG dataset
+# create a MicrosoftAcademicGraph instance to access MAG dataset
 MAG = MicrosoftAcademicGraph(container=MagContainer, account=AzureStorageAccount, key=AzureStorageAccessKey)
 
 # COMMAND ----------
 
-# Load MAG datas
+# load MAG datas
 Papers = MAG.getDataframe('Papers')
 PaperFoses = MAG.getDataframe('PaperFieldsOfStudy')
 Foses = MAG.getDataframe('FieldsOfStudy')
@@ -25,101 +25,109 @@ Affiliations = MAG.getDataframe('Affiliations')
 # COMMAND ----------
 
 # DBTITLE 1,2. Find AI papers and its affiliations
-#Find FieldOfStudyId for Artificial Intelligence
+# find FieldOfStudyId for Artificial Intelligence
 AiId = Foses.select(Foses.FieldOfStudyId).where(Foses.NormalizedName == "artificial intelligence")
 
-#Join PaperFoses to get AI papers
+# join PaperFoses to get AI papers
 AiPapersId = PaperFoses.join(AiId, PaperFoses.FieldOfStudyId == AiId.FieldOfStudyId, 'inner').select(PaperFoses.PaperId)
 
 # COMMAND ----------
 
-#Join Papers to get publish year for AI papers
-AiPapers = Papers.join(AiPapersId, Papers.PaperId == AiPapersId.PaperId, 'inner').select(Papers.PaperId, Papers.Year).where((Papers.Year >= 2010) & (Papers.Year <= 2019))
+# join Papers to get publish year for AI papers
+AiPapers = Papers.where((Papers.Year >= 2010) & (Papers.Year <= 2019)).join(AiPapersId, Papers.PaperId == AiPapersId.PaperId, 'inner').select(Papers.PaperId, Papers.Year)
 
 # COMMAND ----------
 
-#Join PaperAuthorAffiliations to get AffiliationId for AI papers
+# join PaperAuthorAffiliations to get AffiliationId for AI papers
 AiPapersWithAffiId = PaperAuthorAffiliations.join(AiPapers, PaperAuthorAffiliations.PaperId == AiPapers.PaperId, 'inner').select(AiPapers.PaperId, AiPapers.Year, PaperAuthorAffiliations.AffiliationId).where(PaperAuthorAffiliations.AffiliationId.isNotNull()).distinct()
 
 # COMMAND ----------
 
-#Get AffiliationIds
+# get distinct AffiliationIds
 AiAffiliationIds = AiPapersWithAffiId.select("AffiliationId").distinct()
 
-#Join Affiliations to get Affiliation Latitude/Longitude
+# join Affiliations to get affiliation latitude and longitude
 AiAffiliations = Affiliations.join(AiAffiliationIds, AiAffiliationIds.AffiliationId == Affiliations.AffiliationId, 'inner').select(Affiliations.AffiliationId, Affiliations.Latitude, Affiliations.Longitude).where(Affiliations.Latitude.isNotNull()).where(Affiliations.Longitude.isNotNull())
 
 # COMMAND ----------
 
 # DBTITLE 1,3. Get affiliation country info using Geocoder API
+# import libiaries
 import geocoder
 from time import sleep
 
-#Define function to call Geocoder API
-#https://geocoder.readthedocs.io/index.html
-
+# define function to call Bing Maps API from Geocoder
 def getlocations(lat,long):
     bing = geocoder.bing([lat, long], method='reverse')
+    
+    #add 0.1 second time delay to wait API return
     sleep(0.1)
+    
     return bing.country
   
 GenerateLocationsBing_udf_func = udf(getlocations)
 
 # COMMAND ----------
 
-#Get affiliation country info from AiAffiliations
+# get affiliation country info from AiAffiliations
 AiAffiliationsWithCountry = AiAffiliations.select("AffiliationId", "Latitude", "Longitude", GenerateLocationsBing_udf_func("Latitude", "Longitude").alias('Country'))
 
 # COMMAND ----------
 
 # DBTITLE 1,4. Get year and country info for AI papers
-#Join AiPapersWithAffiId to get country info for AI papers
-AiPapersWithLatLongCountry = AiPapersWithAffiId.join(AiAffiliationsWithCountry, AiPapersWithAffiId.AffiliationId == AiAffiliationsWithCountry.AffiliationId, 'inner').select(AiPapersWithAffiId.PaperId, AiPapersWithAffiId.Year, AiPapersWithAffiId.AffiliationId, AiAffiliationsWithCountry.Country)
+# join AiPapersWithAffiId to get country info for AI papers
+AiPapersWithCountry = AiPapersWithAffiId.join(AiAffiliationsWithCountry, AiPapersWithAffiId.AffiliationId == AiAffiliationsWithCountry.AffiliationId, 'inner').select(AiPapersWithAffiId.PaperId, AiPapersWithAffiId.Year, AiPapersWithAffiId.AffiliationId, AiAffiliationsWithCountry.Country)
+
+# COMMAND ----------
+
+# group by Country/Year/PaperId first to eliminate double count when authors of one paper are from different affiliations but in the same country
+AiPapersYearCountryIdGroup = AiPapersWithCountry.groupBy('PaperId', 'Country', 'Year').count()
 
 # COMMAND ----------
 
 # DBTITLE 0,5. Generate AI papers Year-Country count
-#Group by Country/Year to get AI papers count per year and country
-AiPapersYearCountryGroup = AiPapersWithLatLongCountry.groupBy('Country', 'Year').count().withColumnRenamed('count', 'PaperCount')
+# group by Country/Year to get AI papers count per year and country
+AiPapersYearCountryGroup = AiPapersYearCountryIdGroup.groupBy('Country', 'Year').count().withColumnRenamed('count', 'PaperCount')
 
 # COMMAND ----------
 
 # DBTITLE 1,5. Generate AI papers Year-Country Figure
+# import libiaries for drawing figure
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 
 # COMMAND ----------
 
-#Convert Spark dataFrame to Pandas dataFrame for graph drawing
+# convert Spark dataFrame to Pandas dataFrame for graph drawing
 AiPapersYearCountryGroupPandas = AiPapersYearCountryGroup.toPandas()
 
 # COMMAND ----------
 
-#Group by Country to get AI papers count per country
+# group by Country to get AI papers count per country
 AiPapersCountryGroupPandas = AiPapersYearCountryGroupPandas.groupby(['Country'])['PaperCount'].sum().reset_index(name ='PaperCount')
 
-#Get list of top 5 countries
+# get list of top 5 countries
 TopCountryList = AiPapersCountryGroupPandas.nlargest(5, 'PaperCount')['Country'].tolist()
 
-#Filter top countries on AI papers count per year and country
+# filter top countries on AI papers count per year and country
 AiPapersYearCountryGroupTopPandas = AiPapersYearCountryGroupPandas[AiPapersYearCountryGroupPandas['Country'].isin(set(TopCountryList))]
 
 # COMMAND ----------
 
-#Set figures size
+# set figures size
 plt.figure(figsize=(8, 6))
 
-#Set seaborn lineplot
+# set seaborn lineplot
 ax = plt.subplot()
 ax = sns.lineplot(x="Year", y="PaperCount", hue="Country", style="Country", markers=['o','s','v','d', '^'], hue_order = TopCountryList, dashes=False, data=AiPapersYearCountryGroupTopPandas)
 
-#Set figure title
+# set figure title
 ax.set_title("AI Paper Count By Year and Country (Top 5)", fontsize=18)
 
-#Set figure legend
+# set figure legend
 handles, labels = ax.get_legend_handles_labels()
 ax.legend(handles=handles[1:], labels=labels[1:], loc=0, ncol=1, fontsize = 10)
 
-#Display figure
+# display figure
 display(plt.show())
